ae003ff006921132de7ef850726420780f398632
Dominik Engel <domino051@gmail.com>
2020-06-23 11:18:39 +0200

    fix for valdiate on save first round

--- example/index.tsx
+++ example/index.tsx
@@ -2,24 +2,37 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Table from '../.';
+import * as Yup from 'yup';
+
+const AddSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  surname: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  birthYear: Yup.number().required('Required'),
+});
 
 const App = () => {
-
   const [data, setData] = React.useState([
-    { name: "Engel", surname: "Dominik", birthYear: 1994, birthCity: 63 }
-  ])
+    { name: 'Engel', surname: 'Dominik', birthYear: 1994, birthCity: 63 },
+  ]);
   return (
     <div>
       <Table
+        validationSchema={AddSchema}
         columns={[
-          { title: "Name", field: "name" },
-          { title: "First Name", field: "surname" },
-          { title: "Birth Year", field: "birthYear", type: "numeric" },
+          { title: 'Name', field: 'name' },
+          { title: 'First Name', field: 'surname' },
+          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
           {
-            title: "Brith City",
-            field: "birthCity",
-            lookup: { 34: "Aachen", 63: "Berlin" }
-          }
+            title: 'Brith City',
+            field: 'birthCity',
+            lookup: { 34: 'Aachen', 63: 'Berlin' },
+          },
         ]}
         data={data}
         title="Demo Title"
@@ -34,19 +47,22 @@ const App = () => {
           onRowUpdate: (newData, oldData) =>
             new Promise((resolve, reject) => {
               setTimeout(() => {
-                setData(prevData => [...prevData.filter(x => x !== oldData), newData]);
+                setData(prevData => [
+                  ...prevData.filter(x => x !== oldData),
+                  newData,
+                ]);
                 resolve();
               }, 1000);
             }),
           onRowDelete: oldData =>
             new Promise((resolve, reject) => {
               setTimeout(() => {
-
                 setData(prevData => [...prevData.filter(x => x !== oldData)]);
                 resolve();
               }, 1000);
-            })
-        }} />
+            }),
+        }}
+      />
     </div>
   );
 };
